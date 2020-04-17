e133e79f5e0d158bd5d78c19362eddb68669f1d0
Walker Byrnes <walkerbyrnes@gmail.com>
2020-04-17 12:25:10 -0400

    Added routines for worker detection and bin identification

---
+++ src/findBin.py
@@ -0,0 +1,56 @@
+
+
+class WarehouseAisle:
+	# Cooridnate Reference:
+	# X perpendicular to aisle, Y parallel to aisle, Z upwards
+
+
+	def __init__(self, aisleNum, aisleWidth=4.0, binWidth=3.0, binHeight=1.0, binDepth=1.0, aisleLength=20.0, aisleHeight=5.0):
+
+		# Aisle number
+		self.aisleNum = aisleNum
+
+		# These parameters are used to check if the detection is within this aisle
+		self.aisleLength = aisleLength
+		self.aisleHeight = aisleHeight
+		self.aisleWidth = aisleWidth
+
+		# These parameters are used to determine which bin contains the detection
+		self.binWidth = binWidth
+		self.binHeight = binHeight
+		self.binDepth = binDepth
+
+	def findBin(self, coords):
+		""" Find which bin on this aisle is the detection occurring based on the detection's coordinates. """
+
+		# Check the X value to determine which size of the aisle the detection is occurring
+		if 0 < abs(coords[0]) - (self.aisleWidth / 2) < self.binDepth:
+			# Assign side of aisle depending on sign of X coordinate
+			side = "A" if coords[0] < 0 else "B"
+
+		else:
+			print("X Coordinate ", coords[0], " is not within a bin.")
+			return None
+
+		# Check Section from Y coordinate
+		if coords[1] < 0 or coords[1] > self.aisleLength:
+			print("Y Coordinate ", coords[1], " is not within the aisle.")
+			return None
+		else:
+			sect = int(coords[1] / self.binWidth)
+
+		# Check Row from Z coordinate
+		if coords[2] < 0 or coords[2] > self.aisleHeight:
+			print("Z Coordinate ", coords[2], " is not within the aisle.")
+			return None
+		else:
+			row = int(coords[2] / self.binHeight)
+
+		# Construct string from bin location and return
+		binStr = "Aisle " + side + str(self.aisleNum) \
+			+ ", Section " + str(sect) + ", Row " + str(row)
+
+		return binStr
+
+
+
---
+++ src/workerDetection.py
@@ -0,0 +1,89 @@
+
+from imageai.Detection import ObjectDetection
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+class WorkerDetector:
+
+	def __init__(self, basePath, debug=False):
+
+		# Set debug flag
+		self.debug = debug
+
+		# Initialize detector
+		self.detector = ObjectDetection()
+		self.detector.setModelTypeAsRetinaNet()
+    	self.detector.setModelPath( os.path.join(basePath , "data/resnet50_coco_best_v2.0.1.h5"))
+
+    	# Set network to only detect people
+    	self.customObjs = self.detector.CustomObjects(person=True)
+
+    	# Available detection speed options: normal, fast, faster, fastest, flash
+    	self.detector.loadModel(detection_speed='fast')
+
+    	print("Worker Detector ready.")
+
+    def detectFromImage(self, imageRGB):
+
+    	# Perform detection
+    	detImg, detections = self.detector.detectCustomObjectsFromImage(custom_objects=self.customObjs, input_image=imageRGB, 
+    		input_type="array", output_type="array")
+
+    	if self.debug:
+    		print("Detected ", len(detections), " workers.")
+    		plt.imshow(detImg)
+    		plt.show()
+
+    	# Extract bounding boxes from detection dictionaries
+    	bboxes = [d["box_points"] for d in detections]
+
+    	# Return list of bounding boxes
+    	return bboxes
+
+
+""" Procedural test script for detection
+	Author: Muhammad Safwan
+
+"""
+
+#python version 3.6
+#tensorflow version 1.15
+#install following:
+#pip3 install tensorflow==1.15
+#pip3 install opencv-python
+#pip3 install keras
+#pip3 install imageai --upgrade
+#most of the code from https://towardsdatascience.com/object-detection-with-10-lines-of-code-d6cb4d86f606
+#model from https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/resnet50_coco_best_v2.0.1.h5
+
+def detectWorker(execution_path):
+    detector = ObjectDetection()
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+    detector.loadModel()
+
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "6.png"), 
+    	output_image_path=os.path.join(execution_path , "image_6.png"))
+
+    for eachObject in detections:
+        print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+        print("ObjectKeys: ", eachObject.keys())
+
+
+# Testing function
+if __name__ == "__main__":
+    modelPath = "~/catkin_ws/src/CapstoneBinTracking/"
+    wd = WorkerDetector(modelPath)
+
+    # Import test image
+    imagePath = ""  # TEST IMAGE PATH
+    img = np.asarray(Image.open(imagePath))
+    detections = wd.detectFromImage(img)
+
+    # Print out detections
+    print("Detections: \n", detections)
+
+
